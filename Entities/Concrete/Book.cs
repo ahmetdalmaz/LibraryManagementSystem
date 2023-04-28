e3295df9cbfa914f0bd34c278f8fa87fd51089a9
ahmetdalmaz <ahmett3437@hotmail.com>
2023-04-28 22:04:43 +0300

    Add project files.

---
+++ Entities/Concrete/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public int CategoryId { get; set; }
+        public string BookName { get; set; }
+        public int PageCount { get; set; }
+
+    }
+}
